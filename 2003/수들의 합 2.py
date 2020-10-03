2f8353ad83b33efdc0d4d936aebb60e92c7d1b14
Jeong Jong Woo <knight7024@naver.com>
2020-10-03 10:27:15 +0900

    Update 수들의 합 2.py

--- 2003/수들의 합 2.py
+++ 2003/수들의 합 2.py
@@ -1,3 +1,6 @@
+# 투 포인터 풀이
+# O(N)
+
 n, m = map(int, input().split())
 arr = list(map(int, input().split()))
 
@@ -25,3 +28,32 @@ while front < n and end < n:
 			sum += arr[end]
 
 print(ans)
+
+'''
+# 부분합 + 이분탐색 풀이
+# O(NlogN)
+
+n, m = map(int, input().split())
+arr = [0]
+arr.extend(list(map(int, input().split())))
+
+for i in range(n):
+	arr[i + 1] += arr[i]
+
+ans = 0
+for i in range(0, n):
+	start = i
+	end = n
+	while start <= end:
+		mid = (start + end) // 2
+		ret = arr[mid] - arr[i]
+		if ret == m:
+			ans += 1
+			break
+		elif ret < m:
+			start = mid + 1
+		else:
+			end = mid - 1
+
+print(ans)
+'''
